1818ff571ea753ea2cd84b916b71f2d0c20fe3c3
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-08-10 19:39:13 -0700

    fixes update route, changes update to patch

--- routes/dogRouter.js
+++ routes/dogRouter.js
@@ -7,6 +7,6 @@ router.post("/", auth, createDog);
 router.get("/", getDogs);
 router.get("/:id", getDog);
 router.delete("/:id", auth, deleteDog);
-router.update("/:id", auth, updateDog)
+router.patch("/:id", auth, updateDog)
 router.get("/userdogs/:id", auth, getDogsByUser)
 export default router;
