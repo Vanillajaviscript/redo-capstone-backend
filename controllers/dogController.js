bc4a0f7315482fa90d017956fa2dd7908c608475
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-08-10 17:24:11 -0700

    adds endpoint for user created dogs

--- controllers/dogController.js
+++ controllers/dogController.js
@@ -1,5 +1,6 @@
 import express from "express";
 import DogModal from "../models/dogSchema.js";
+import mongoose from "mongoose";
 
 export const createDog = async (req, res) => {
   const dog = req.body;
@@ -37,4 +38,14 @@ export const getDog = async (req, res) => {
   } catch (error) {
     res.status(404).json({ message: "Bad Request!" });
   }
+};
+
+export const getDogsByUser = async (req, res) => {
+  const {id} = req.params;
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({message: "User does not exist"});
+  };
+  //gets all dogs based on who created them
+  const userDogs = await DogModal.find({creator: id});
+  res.status(200).json(userDogs);
 };
--- routes/dogRouter.js
+++ routes/dogRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
 const router = express.Router();
-import { createDog, getDogs, getDog } from "../controllers/dogController.js";
+import { createDog, getDogs, getDog, getDogsByUser } from "../controllers/dogController.js";
 import auth from "../middleware/auth.js";
 
 router.post("/", auth, createDog);
 router.get("/", getDogs);
 router.get("/:id", getDog)
-
+router.get("/userdogs/:id", auth, getDogsByUser)
 export default router;
